3939812aee37bfd91b7e73ad7bf82253682b632a
zayan <122380059+zayanzz@users.noreply.github.com>
2024-04-08 12:21:26 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,96 @@
 function showSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex';
-  }
-  function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
-  }
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const sidebarLinks = document.querySelectorAll('.sidebar a');
-    sidebarLinks.forEach(function (link) {
-      link.addEventListener('click', function () {
-        hideSidebar();
-      });
+document.addEventListener('DOMContentLoaded', function () {
+  const sidebarLinks = document.querySelectorAll('.sidebar a');
+  sidebarLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      hideSidebar();
     });
   });
+});
 
-  // slider 
+// slider 
 
-  const slides = document.querySelectorAll('.slider');
-  let currentIndex = 0;
-  let intervalId;
-  
-  function showSlide(index) {
-    slides.forEach((slide, i) => {
-      if (i === index) {
-        slide.classList.add('active');
-      } else {
-        slide.classList.remove('active');
-      }
-    });
-  }
-  
-  function nextSlide() {
-    clearInterval(intervalId);
-    currentIndex = (currentIndex + 1) % slides.length;
-    showSlide(currentIndex);
-    intervalId = setInterval(nextSlide, 6000);
-  }
-  
-  function prevSlide() {
-    clearInterval(intervalId);
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    showSlide(currentIndex);
-    intervalId = setInterval(nextSlide, 6000);
-  }
-  
-  function startSlider() {
-    intervalId = setInterval(nextSlide, 6000);
-  }
-  
-  function stopSlider() {
-    clearInterval(intervalId);
-  }
-  
-  // Start slider on load
-  startSlider();
-  
+const slides = document.querySelectorAll('.slider');
+let currentIndex = 0;
+let intervalId;
 
-  // / slider animation ///////////
-  const swiftUpElements = document.querySelectorAll('.swift-up-text');
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
 
-  swiftUpElements.forEach(elem => {
-  
-    const words = elem.textContent.split(' ');
-    elem.innerHTML = '';
-  
-    words.forEach((el, index) => {
-      words[index] = `<span><i>${words[index]}</i></span>`;
-    });
-  
-    elem.innerHTML = words.join(' ');
-  
-    const children = document.querySelectorAll('span > i');
-    children.forEach((node, index) => {
-      node.style.animationDelay = `${index * .1}s`;
-    });
+function nextSlide() {
+  clearInterval(intervalId);
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+  intervalId = setInterval(nextSlide, 6000);
+}
+
+function prevSlide() {
+  clearInterval(intervalId);
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(currentIndex);
+  intervalId = setInterval(nextSlide, 6000);
+}
+
+function startSlider() {
+  intervalId = setInterval(nextSlide, 6000);
+}
+
+function stopSlider() {
+  clearInterval(intervalId);
+}
+
+// Start slider on load
+startSlider();
+
+
+// / slider animation ///////////
+const swiftUpElements = document.querySelectorAll('.swift-up-text');
+
+swiftUpElements.forEach(elem => {
+
+  const words = elem.textContent.split(' ');
+  elem.innerHTML = '';
+
+  words.forEach((el, index) => {
+    words[index] = `<span><i>${words[index]}</i></span>`;
   });
 
+  elem.innerHTML = words.join(' ');
 
-  document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        var elements = document.querySelectorAll('.animate');
+  const children = document.querySelectorAll('span > i');
+  children.forEach((node, index) => {
+    node.style.animationDelay = `${index * .1}s`;
+  });
+});
 
-        elements.forEach(function(element) {
-            element.classList.add('animated');
-        });
-    }, 500); // Adjust the delay (in milliseconds) as needed
+
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    var elements = document.querySelectorAll('.animate');
+
+    elements.forEach(function (element) {
+      element.classList.add('animated');
+    });
+  }, 500); // Adjust the delay (in milliseconds) as needed
 });
 
 // sales team  
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var scrollTop = window.scrollY;
   var bgImg = document.querySelector('.bg-img');
   bgImg.style.backgroundPosition = 'center ' + (-scrollTop / 3) + 'px';
@@ -107,3 +107,6 @@ document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
 function error_call_submitation_failed() {
   alert('404 Error currently not available contact form please contact +97121892932')
 }
+
+
+
